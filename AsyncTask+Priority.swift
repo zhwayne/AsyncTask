c13ffc5172ccd11f1c9b58a5663611c6d2213920
iya <mrzhwayne@163.com>
2021-09-17 17:35:13 +0800

    initinal commit

--- AsyncTask+Priority.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AsyncTask+Priority.swift
-//  AsyncTask
-//
-//  Created by 张尉 on 2021/9/17.
-//
-
-import Foundation
--- AsyncTask+State.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AsyncTask+State.swift
-//  AsyncTask
-//
-//  Created by 张尉 on 2021/9/17.
-//
-
-import Foundation
