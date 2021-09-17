a0af2b693370ff2c574910a003dca46f6b9005d2
iya <mrzhwayne@163.com>
2021-09-17 19:11:19 +0800

    demo 调整

--- Example/AsyncTask/ViewController.swift
+++ Example/AsyncTask/ViewController.swift
@@ -17,28 +17,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let t1 = AsyncTask { task in
-            print("t1 start")
-            Thread.sleep(forTimeInterval: 1)
-            print("t1 end")
-            task.finish()
+        let tasks = (0..<10).map { idx -> AsyncTask in
+            let task = AsyncTask(priority: .custom(idx)) { task in
+                print("t\(idx) start")
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    print("t\(idx) end")
+                    task.finish()
+                }
+            }
+            return task
         }
         
-        let t2 = AsyncTask(priority: .hight)  { task in
-            print("t2 start")
-            Thread.sleep(forTimeInterval: 1)
-            print("t2 end")
-            task.finish()
-        }
-        
-        let t3 = AsyncTask(priority: .custom(520))  { task in
-            print("t3 start")
-            Thread.sleep(forTimeInterval: 1)
-            print("t3 end")
-            task.finish()
-        }
-        
-        queue.add(tasks: [t1, t2, t3])
+        queue.add(tasks: tasks)
     }
 
     override func didReceiveMemoryWarning() {
